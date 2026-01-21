b0751cb460ef6ab2914173b12d8a8d6614747248
devzeuz <soceeenin@yahoo.com>
2026-01-21 01:21:48 -0800

    Changed billing mode

--- modules/DynamoDB/main.tf
+++ modules/DynamoDB/main.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "techx-main-table"{
     name           = "techx-main-table"
-    billing_mode   = "ON_DEMAND"
+    billing_mode   = "PAY_PER_REQUEST" //Billing mode is set to pay per request and provisoned 
 
     // defining the attributes or primary key
     hash_key       = "PK"
