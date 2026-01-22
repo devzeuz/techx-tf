76f4c5fa37bddfebeebcb13f2640dd32ba337ea5
devzeuz <soceeenin@yahoo.com>
2026-01-22 07:11:51 -0800

    Change modules path for lambda

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -1,6 +1,6 @@
 data "archive_file" "techx-lambda-zip" {
   type        = "zip"
-  source_dir = "${path.module}/src/lambda-techx-function.py"
+  source_dir = "${path.module}/src/"
   output_path = "${path.module}/src/lambda-techx-function.zip"
 }
 
