bfa5ad825ef216b85c85e7d5f308bae87d52b78c
devzeuz <soceeenin@yahoo.com>
2026-01-22 10:55:25 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -8,8 +8,16 @@ resource "aws_api_gateway_rest_api" "techx-tf-api-gateway" {
 
 resource "aws_api_gateway_resource" "techx-tf-courses-resource" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-    parent_id   = aws_api_gateway_rest_api.techx-tf-api-gateway.root_resource_id
+    parent_id   = aws_api_gateway_rest_api.techx-tf-api-gateway.root_resource_id // root resource id represent the / aprt of the rest api
     path_part   = "courses"
+}
+
+output "root_resource_id"{
+    value = aws_api_gateway_rest_api.techx-tf-api-gateway.root_resource_id
+}
+
+output "restApi_id"{
+    value = aws_api_gateway_rest_api.techx-tf-api-gateway.id
 }
 
 resource "aws_api_gateway_method" "techx-tf-courses-method" {
