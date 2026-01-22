6016ccce15d179e8ea0ff5b53ffa2778e8110394
devzeuz <soceeenin@yahoo.com>
2026-01-22 10:37:50 -0800

    api

--- module/api/main.tf
+++ module/api/main.tf
@@ -6,15 +6,9 @@ resource "aws_api_gateway_rest_api" "techx-tf-api-gateway" {
     }
 }
 
-resource "aws_api_gateway_resource" "techx-tf-api-resource" {
-    rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-    parent_id   = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-    path_part   = "/"
-}
-
 resource "aws_api_gateway_resource" "techx-tf-courses-resource" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-    parent_id   = aws_api_gateway_resource.techx-tf-api-resource.id
+    parent_id   = aws_api_gateway_resource.techx-tf-api-gateway.id
     path_part   = "/courses"
 }
 
