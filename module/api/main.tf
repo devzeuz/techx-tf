da0d9d90564f38ce6b700d43cca65b86563c9c56
devzeuz <soceeenin@yahoo.com>
2026-01-26 21:25:20 -0800

    API ID Method Integration

--- module/api/main.tf
+++ module/api/main.tf
@@ -21,20 +21,28 @@ resource "aws_api_gateway_resource" "techx-tf-id-resource" {
 
 // Resources for API gateway
 
+// API resource Methods
 resource "aws_api_gateway_method" "techx-tf-courses-method" {
     rest_api_id   = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id   = aws_api_gateway_resource.techx-tf-courses-resource.id
     http_method   = "GET"
     authorization  = "NONE"
-
-    // I will be getting the source ARN from this method, that goes in the lambda permission.
 }
+
+resource "aws_api_gateway_method" "techx-tf-id-method" {
+    rest_api_id   = aws_api_gateway_rest_api.techx-tf-api-gateway.id
+    resource_id   = aws_api_gateway_resource.techx-tf-id-resource.id
+    http_method = "GET"
+    authorization = "NONE"
+}
+// API Resource Methods
+
 
 resource "aws_api_gateway_integration" "techx-tf-courses-integration" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id = aws_api_gateway_resource.techx-tf-courses-resource.id
     http_method = aws_api_gateway_method.techx-tf-courses-method.http_method
-    integration_http_method = "POST" // API gateway POST the frontend request to lambda therefor invoking it in the process
+    # integration_http_method = "POST" // API gateway POST the frontend request to lambda therefor invoking it in the process, this is why integration HTTP method is set to post
     type                    = "AWS_PROXY"
     uri                     = var.lambda-invoke-arn
 }
