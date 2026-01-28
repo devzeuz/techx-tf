dc68e7e9254c235d6b1823c374375e2c9ad96bbd
devzeuz <soceeenin@yahoo.com>
2026-01-27 21:18:53 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -68,7 +68,7 @@ resource "aws_api_gateway_method_response" "techx-tf-courses-method-response" {
 
 resource "aws_api_gateway_method_response" "techx-tf-id-method-response" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-    resource_id = aws_api_gateway_resource.techx-tf-id.resource.id
+    resource_id = aws_api_gateway_resource.techx-tf-id-resource.id
     http_method = aws_api_gateway_method.techx-tf-id-method.http_method
     status_code = "200"
 }
