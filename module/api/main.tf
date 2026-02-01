d9f7078198937b7d15d862e96729c86ccb3e6026
devzeuz <soceeenin@yahoo.com>
2026-02-01 09:05:53 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -66,7 +66,7 @@ resource "aws_api_gateway_method" "techx-tf-user-method" {
 resource "aws_api_gateway_integration" "techx-tf-user-options-integration" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
-    http_method = "OPTIONS"
+    http_method = aws_api_gateway_method.techx-tf-user-method.http_method
     type                    = "MOCK"
 }
 // API Resource Methods
