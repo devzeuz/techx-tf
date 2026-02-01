caaea0a3e7497a2d6c9ddd860de51f7b8f3ad24c
devzeuz <soceeenin@yahoo.com>
2026-02-01 08:56:32 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -55,7 +55,14 @@ resource "aws_api_gateway_method" "techx-tf-user-get-method" {
      http_method = "GET"
      authorization = "NONE" // NONE is a placeholder, there is actually auth token expected. since access (to DynamoDB) is involved.
 }
-             // OPTIONS method for /user resource
+             // OPTIONS method/integration for /user resource
+resource "aws_api_gateway_method" "techx-tf-user-method" {
+    rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
+     resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
+     http_method = "OPTIONS"
+     authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "techx-tf-user-options-integration" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
