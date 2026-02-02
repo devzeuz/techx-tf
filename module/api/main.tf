39cf3f9122641197e2019b300d6712db9f7e6761
devzeuz <soceeenin@yahoo.com>
2026-02-02 09:09:08 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -104,7 +104,7 @@ resource "aws_api_gateway_integration" "techx-tf-id-integration" {
 resource "aws_api_gateway_integration" "techx-tf-user-get-integration" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
-    http_method = aws_api_gateway_method.techx-tf-user-method.http_method
+    http_method = aws_api_gateway_method.techx-tf-user-get-method.http_method
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
     uri                     = var.lambda-invoke-arn
