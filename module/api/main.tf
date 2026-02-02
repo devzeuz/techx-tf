d3eabe3939dc291fba15fb2c5775bf99f6c6b63d
devzeuz <soceeenin@yahoo.com>
2026-02-02 09:42:05 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -57,12 +57,12 @@ resource "aws_api_gateway_method" "techx-tf-user-get-method" {
 }
 
 // OPTIONS method for /user resource
-resource "aws_api_gateway_method" "techx-tf-user-options-method" {
-    rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
-     resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
-     http_method = "OPTIONS"
-     authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "techx-tf-user-options-method" {
+#     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
+#      resource_id = aws_api_gateway_resource.techx-tf-user-resource.id
+#      http_method = "OPTIONS"
+#      authorization = "NONE"
+# }
 
 // OPTIONS method for /courses/{id} resource
 resource "aws_api_gateway_method" "techx-tf-id-options-method" {
