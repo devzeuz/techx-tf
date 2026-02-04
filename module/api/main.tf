884ecddd73acd19853bcd9e08e5f68f8c0ce6059
devzeuz <soceeenin@yahoo.com>
2026-02-04 08:46:50 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -252,7 +252,7 @@ resource "aws_api_gateway_integration_response" "techx-tf-courses-options-integr
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
         "method.response.header.Access-Control-Allow-Methods" = "'GET','OPTIONS'",
-        "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
     }
 }
 //API gateway INTEGRATION responses
