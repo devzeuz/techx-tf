7b37a2a5c20918c7b62c73e8bd8ca5ec62007a4e
devzeuz <soceeenin@yahoo.com>
2026-02-03 21:17:45 -0800

    script commit

--- module/cognito/main.tf
+++ module/cognito/main.tf
@@ -10,6 +10,18 @@ resource "aws_cognito_user_pool" "techx-tf-user-pool" {
     }
 }
 
+# resource "aws_cognito_user_pool_client" "techx-tf-user-pool-client" {
+#     name         = "techx-tf-user-pool-client"
+#     user_pool_id = aws_cognito_user_pool.techx-tf-user-pool.id
+#     generate_secret = false
+
+#     explicit_auth_flows = [
+#         "ALLOW_USER_PASSWORD_AUTH",   # allows username/password login
+#         "ALLOW_USER_SRP_AUTH",        # allows SRP login
+#         "ALLOW_REFRESH_TOKEN_AUTH"    # allows token refresh
+#     ]
+# }
+
 resource "aws_cognito_user_pool_client" "techx-tf-user-pool-client" {
     name         = "techx-tf-user-pool-client"
     user_pool_id = aws_cognito_user_pool.techx-tf-user-pool.id
