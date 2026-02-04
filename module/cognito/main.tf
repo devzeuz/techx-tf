d1d2eb5833369d9de0bc59968b4d195a631d03f6
devzeuz <soceeenin@yahoo.com>
2026-02-03 21:12:07 -0800

    script commit

--- module/cognito/main.tf
+++ module/cognito/main.tf
@@ -13,16 +13,11 @@ resource "aws_cognito_user_pool" "techx-tf-user-pool" {
 resource "aws_cognito_user_pool_client" "techx-tf-user-pool-client" {
     name         = "techx-tf-user-pool-client"
     user_pool_id = aws_cognito_user_pool.techx-tf-user-pool.id
-    generate_secret            = false
-
-    allowed_oauth_flows_user_pool_client = true
-    allowed_oauth_flows                  = ["code"]       # initial code flow
-    allowed_oauth_scopes                 = ["openid", "email"]
-    supported_identity_providers         = ["COGNITO"]
+    generate_secret = false
 
     explicit_auth_flows = [
-        "ALLOW_USER_PASSWORD_AUTH",        # allows password login
-        "ALLOW_USER_SRP_AUTH",             # allows SRP (secure remote password)
-        "ALLOW_REFRESH_TOKEN_AUTH"         # allows token refresh
+        "ALLOW_USER_PASSWORD_AUTH",   # allows username/password login
+        "ALLOW_USER_SRP_AUTH",        # allows SRP login
+        "ALLOW_REFRESH_TOKEN_AUTH"    # allows token refresh
     ]
 }
