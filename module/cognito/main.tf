eeb8d72d20bba97834489a081220791a45698a99
devzeuz <soceeenin@yahoo.com>
2026-02-04 01:35:16 -0800

    script commit

--- module/cognito/main.tf
+++ module/cognito/main.tf
@@ -1,9 +1,9 @@
-resource "aws_cognito_user_pool" "techx-tf-user-pool" {
+resource "aws_cognito_user_pool" "techx-tf-cognito-user-pool" {
     name = "techx-tf-cognito-user-pool"
     alias_attributes = ["email"] // I justchnaged this so the next step is to push and run. remeber to change the ids on the frontend code.
 }
 
-resource "aws_cognito_user_pool_client" "techx-tf-user-pool-client" {
-    name         = "techx-tf-user-pool-client"
-    user_pool_id = aws_cognito_user_pool.techx-tf-user-pool.id
+resource "aws_cognito_user_pool_client" "techx-tf-cognito-user-pool-client" {
+    name         = "techx-tf-cognito-user-pool-client"
+    user_pool_id = aws_cognito_user_pool.techx-tf-cognito-user-pool.id
 }
--- module/cognito/output.tf
+++ module/cognito/output.tf
@@ -1,3 +1,3 @@
 output "cognito-arn-output" {
-    value = aws_cognito_user_pool.techx-tf-user-pool.arn
+    value = aws_cognito_user_pool.techx-tf-cognito-user-pool.arn
 }
