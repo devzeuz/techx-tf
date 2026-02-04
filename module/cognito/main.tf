684b90835ee5de2580afc0b00d01d50dd8df44f1
devzeuz <soceeenin@yahoo.com>
2026-02-04 01:20:49 -0800

    script commit

--- module/cognito/main.tf
+++ module/cognito/main.tf
@@ -1,7 +1,6 @@
 resource "aws_cognito_user_pool" "techx-tf-user-pool" {
     name = "techx-tf-user-pool"
-    alias_attributes = ["preferred_username"] // I justchnaged this so the next step is to push and run. remeber to change the ids on the frontend code.
-    # auto_verified_attributes = ["email"]
+    alias_attributes = ["email"] // I justchnaged this so the next step is to push and run. remeber to change the ids on the frontend code.
 }
 
 resource "aws_cognito_user_pool_client" "techx-tf-user-pool-client" {
