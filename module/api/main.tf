9b40a2b9df99d9c1335e1ab14cd5bcfcd61a07b5
devzeuz <soceeenin@yahoo.com>
2026-02-04 02:39:37 -0800

    script commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -202,7 +202,7 @@ resource "aws_api_gateway_integration_response" "techx-tf-user-options-integrati
 
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,POST'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,POST','OPTIONS'",
         "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     }
 }
@@ -216,11 +216,11 @@ resource "aws_api_gateway_integration_response" "techx-tf-id-options-integration
 
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET','OPTIONS'",
         "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     }
 }
-// /course OPTIONS methos integration-response
+// /course OPTIONS method integration-response
 resource "aws_api_gateway_integration_response" "techx-tf-courses-options-integration-response" {
     rest_api_id = aws_api_gateway_rest_api.techx-tf-api-gateway.id
     resource_id = aws_api_gateway_resource.techx-tf-courses-resource.id
@@ -229,7 +229,7 @@ resource "aws_api_gateway_integration_response" "techx-tf-courses-options-integr
 
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET','OPTIONS'",
         "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     }
 }
